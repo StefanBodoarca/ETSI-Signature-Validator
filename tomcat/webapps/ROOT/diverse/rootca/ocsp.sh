b32a21a1c2820571bbb27c305348b4e8dc07595c
stef stef <stef@example.com>
2020-07-24 08:31:02 +0300

    - tomcat

--- tomcat/webapps/ROOT/diverse/rootca/ocsp.sh
+++
@@ -1,4 +0,0 @@
-file_name="ocsp3"
-openssl genrsa -aes256 -out ./private/$file_name".key" 2048
-openssl req -new -key ./private/$file_name".key" -out ./csr/$file_name".csr" -config rootca.conf
-openssl ca -in ./csr/$file_name".csr" -out ./certs/$file_name".crt" -cert root-ca.crt -keyfile ./private/root-ca.key -config rootca.conf -extensions v3_OCSP
